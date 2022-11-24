ccd0f432b061f926ba9adaa21d61969c998cd299
Dilec Padovani <39855725+DilecPadovani@users.noreply.github.com>
2022-11-24 22:58:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n < 2)
+    {
+        cout << "1" << endl;
+        return 0;
+    };
+    int a = 1, b = 0, c = 0;
+
+    while (1 == 1)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        if (c <= n)
+        {
+            cout << c << endl;
+            continue;
+        }
+        break;
+    }
 }
