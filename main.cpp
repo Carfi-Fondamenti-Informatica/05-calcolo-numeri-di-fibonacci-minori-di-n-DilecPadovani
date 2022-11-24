4e5af05d4a5dcd2fa5ccffef938eb80e861308df
Dilec Padovani <39855725+DilecPadovani@users.noreply.github.com>
2022-11-24 23:04:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ int main()
     int n;
     cin >> n;
 
-    if (n <= 2)
+    if (n =1)
     {
         cout << "1" << endl;
+        return 0;
+    };
+       if (n =2)
+    {
+        cout << "1" << endl<<"1"<<endl;
         return 0;
     };
     int a = 1, b = 0, c = 0;
